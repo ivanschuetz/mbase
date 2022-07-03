bf1c2f7b6daab67c18663bc550d8bf4ff0d30be7
Ivan Schuetz <ivanschuetz@gmail.com>
2022-07-03 21:55:10 +0200

    Add convenience to convert timestamp to date

--- src/models/timestamp.rs
+++ src/models/timestamp.rs
@@ -1,4 +1,7 @@
-use chrono::{DateTime, Utc};
+use std::convert::TryInto;
+
+use anyhow::Result;
+use chrono::{DateTime, NaiveDateTime, Utc};
 use serde::{Deserialize, Serialize};
 
 /// Unix timestamp (seconds)
@@ -8,5 +11,13 @@ pub struct Timestamp(pub u64);
 impl From<DateTime<Utc>> for Timestamp {
     fn from(dt: DateTime<Utc>) -> Self {
         Timestamp(dt.timestamp() as u64)
+    }
+}
+
+impl Timestamp {
+    pub fn to_date(&self) -> Result<DateTime<Utc>> {
+        let timestamp_i64 = self.0.try_into()?;
+        let naive = NaiveDateTime::from_timestamp(timestamp_i64, 0);
+        Ok(DateTime::from_utc(naive, Utc))
     }
 }
